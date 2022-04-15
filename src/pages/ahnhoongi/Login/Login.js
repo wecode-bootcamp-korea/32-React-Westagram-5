3736d445e4e16e01866609b64c1f9e84106a7da9
Hoon Gi Ahn <ahg4870@gmail.com>
2022-04-15 20:45:05 +0900

    Add: Mission 2 - 로그인 버튼 활성화 기능 구현

--- src/pages/ahnhoongi/Login/Login.js
+++ src/pages/ahnhoongi/Login/Login.js
@@ -2,20 +2,28 @@ import React, { useState } from 'react';
 import './Login.scss';
 import { useNavigate } from 'react-router-dom';
 
-function Login() {
-  const [IdInput, setIdInput] = useState('');
-
-  function handleIdInput(event) {
-    setIdInput(event.target.value);
-  }
-
-  const [Pwinput, setPwInput] = useState('');
-
-  function handlePwInput(event) {
-    setPwInput(event.target.value);
-  }
-
+const Login = () => {
   const navigate = useNavigate();
+
+  const [idInput, setIdInput] = useState('');
+
+  const handleIdInput = event => {
+    setIdInput(event.target.value);
+  };
+
+  const [pwInput, setPwInput] = useState('');
+
+  const handlePwInput = event => {
+    setPwInput(event.target.value);
+  };
+
+  const [isActive, setIsActive] = useState(false);
+
+  const active = () => {
+    idInput.includes('@') && pwInput.length > 4
+      ? setIsActive(true)
+      : setIsActive(false);
+  };
 
   const goToMain = () => {
     navigate('/main-ahnhoongi');
@@ -33,14 +41,21 @@ function Login() {
             className="id"
             placeholder="전화번호, 사용자 이름 또는 이메일"
             onChange={handleIdInput}
+            onKeyUp={active}
           />
           <input
             type="password"
             className="pw"
             placeholder="비밀번호"
             onChange={handlePwInput}
+            onKeyUp={active}
           />
-          <button type="button" className="bt" onClick={goToMain} /*disabled*/>
+          <button
+            type="button"
+            className={`bt ${isActive ? 'abled' : 'disabled'}`}
+            onClick={goToMain}
+            disabled={isActive ? false : true}
+          >
             로그인
           </button>
         </form>
@@ -50,6 +65,6 @@ function Login() {
       </main>
     </body>
   );
-}
+};
 
 export default Login;
