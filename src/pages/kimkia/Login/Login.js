41cd1e8ac454ce939b3ed20ac4bcd167861d1ee0
guiahkim <hnmpot0318@naver.com>
2022-04-18 19:28:42 +0900

    Mission 1 clear

--- src/pages/kimkia/Login/Login.js
+++ src/pages/kimkia/Login/Login.js
@@ -4,8 +4,17 @@ import { useNavigate } from 'react-router-dom';
 
 function Login() {
   const navigate = useNavigate();
+
   function goToMain() {
     navigate('/main-kia');
+  }
+
+  function handleIdInput(E) {
+    let IdValue = E.target.value;
+  }
+
+  function handlePWInput(E) {
+    let PWValue = E.target.value;
   }
 
   return (
@@ -13,12 +22,18 @@ function Login() {
       <div className="insideBox">
         <h1>westagram</h1>
         <input
+          onChange={handleIdInput}
           className="phone"
           type="text"
           placeholder="전화번호, 사용자 이름 또는 이메일"
         />
         <br />
-        <input className="password" type="text" placeholder="비밀번호" />
+        <input
+          onChange={handlePWInput}
+          className="password"
+          type="password"
+          placeholder="비밀번호"
+        />
         <br />
         <br />
         <button onClick={goToMain}>로그인</button>
